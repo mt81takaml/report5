505819d7d4726b3f93de82ba0e4ff2e57ba2f336
takaml <b2123mt@aiit.ac.jp>
2021-08-03 23:30:16 +0900

    レポート5提出用

--- gcd-test.sh
+++ gcd-test.sh
@@ -3,7 +3,7 @@
 ans="/tmp/$$-ans"
 result="/tmp/$$-result"
 
-#正常動作、異常動作の確認で想定しない挙動をしたときに、エラー処理処理とtmpファイルの後処理をする関数。
+#正常動作、異常動作の確認で想定しない挙動をしたときに、エラー処理とtmpファイルの後処理をする関数。
 ERROR_EXIT(){
    echo $1
    rm /tmp/$$-*
@@ -23,13 +23,14 @@ echo "100と250の最大公約数は50" > ${ans}
 ./gcd.sh 100 250 > ${result} 
 diff ${ans} ${result} && SUCCESS "test 1-1 OK" || ERROR_EXIT "error in 1-1" 
 
-#1-2 大きい自然数を入力して正常に動作するかチェック
+#1-2　少し大きい自然数を入力して正常に動作するかチェック
 echo "10000と1500の最大公約数は500" > ${ans}
 ./gcd.sh 10000 1500 > ${result}
 diff ${ans} ${result} && SUCCESS "test 1-2 OK" || ERROR_EXIT "error in 1-2"
 
 
 #異常動作の確認
+#2-1では、引数の数によって起きるエラーを想定する。
 #2-1(A) 入力を一つだけにしたときにエラーが正しく出るかチェック
 echo "ERROR: PLZ INPUT 2 ARGMENTS" > ${ans}
 ./gcd.sh 20 > ${result}  && ERROR_EXIT "error before diff in 2-1" 
@@ -38,20 +39,28 @@ diff ${ans} ${result} && SUCCESS "test 2-1(A) OK" || ERROR_EXIT "error in 2-1(A)
 #2-1(B) 入力が10つあった場合に、エラーが正しく出るかチェック
 echo "ERROR: PLZ INPUT 2 ARGMENTS" > ${ans}
 ./gcd.sh a b c d e f g h i j > ${result}  && ERROR_EXIT "error before diff in 2-1"
-diff ${ans} ${result} && SUCCESS "test 2-1(B) OK" || ERROR_EXIT  "error in 2-1(B)" 
+diff ${ans} ${result} && SUCCESS "test 2-1(B) OK" || ERROR_EXIT "error in 2-1(B)" 
 
 
-#2-2 文字を入力してエラーが正しく出るかチェック
-echo "ERROR: PLZ INPUT 2 FIGURES" > ${ans}
-./gcd.sh 最大 公約数 > ${result}  && ERROR_EXIT "error before diff in 2-2" 
-diff ${ans} ${result} && SUCCESS "test 2-2 OK" || ERROR_EXIT "error in 2-2" 
+#2-2 文字の入力、引数の桁の限界、小数の入力によって起きるエラーを想定する。
+#2-2(A) 文字が入力されたときにエラーが正しく出るかチェック。
+echo "ERROR: PLZ INPUT TWO NATURAL NUMBERS SMALLER THAN 21 DIGITS" > ${ans}
+./gcd.sh 最大 公約数 > ${result}  && ERROR_EXIT "error before diff in 2-2(A)" 
+diff ${ans} ${result} && SUCCESS "test 2-2(A) OK" || ERROR_EXIT "error in 2-2(A)" 
 
-#2-3 因数の数字の桁が21桁以上になった場合にエラーが正しく出るかチェック
- echo "ERROR: PLZ INPUT 2 FIGURES" > ${ans}
-./gcd.sh 22222222222222222222 1234 > ${result}  && ERROR_EXIT "error before diff in 2-3"
-diff ${ans} ${result} && SUCCESS "test 2-3 OK" || ERROR_EXIT "error in 2-3"
+#2-2(B) 入力した引数の桁によっておきるエラーを想定する。
+#引数の数字の桁が21桁以上になった場合にエラーが正しく出るかチェック
+echo "ERROR: PLZ INPUT TWO NATURAL NUMBERS SMALLER THAN 21 DIGITS" > ${ans}
+ ./gcd.sh 22222222222222222222 1234 > ${result}  && ERROR_EXIT "error before diff in 2-2(B)"
+diff ${ans} ${result} && SUCCESS "test 2-2(B) OK" || ERROR_EXIT "error in 2-2(B)"
 
-#2-4 自然数以外を入力して、エラーが正しく出るかチェック
+#2-2(C) 小数を入力して、エラーが正しく出るかチェック
+echo "ERROR: PLZ INPUT TWO NATURAL NUMBERS SMALLER THAN 21 DIGITS" > ${ans}
+./gcd.sh 1.5 -1 > ${result}  && ERROR_EXIT "error before diff in 2-2(C)"
+diff ${ans} ${result} && SUCCESS "test 2-2(C) OK" || ERROR_EXIT "error in 2-2(C)"
+
+#2-3 自然数以外の数字を入れたことによるエラーを想定する。
+#2-3 マイナスの値を入力して、エラーが正しく出るかチェック
 echo "ERROR: PLZ INPUT NATURAL NUMBER" > ${ans}
-./gcd.sh 1 -1 > ${result}  && ERROR_EXIT "error before diff in 2-4" 
-diff ${ans} ${result} && SUCCESS "test 2-4 OK" ||  ERROR_EXIT "error in 2-4"
+./gcd.sh 1 -1 > ${result}  && ERROR_EXIT "error before diff in 2-3(A)" 
+diff ${ans} ${result} && SUCCESS "test 2-3 OK" || ERROR_EXIT "error in 2-3 OK"
--- gcd.sh
+++ gcd.sh
@@ -9,13 +9,13 @@ then echo "ERROR: PLZ INPUT 2 ARGMENTS"
  exit 1
 fi
 
-#2-2　文字を入力した時にエラー文を出す。
+#2-2　文字、21桁より多い数字、小数を入力をした時にエラー文を出す。
 if ! [ "$n" -eq "$n" -a  "$m" -eq "$m" ] 2>/dev/null ;
-then echo "ERROR: PLZ INPUT 2 FIGURES"
+then echo "ERROR: PLZ INPUT TWO NATURAL NUMBERS SMALLER THAN 21 DIGITS"
  exit 1
 fi
 
-#2-3 自然数以外の数字を入力したときにエラー文を出す。
+#2-3 マイナスの数字を入力したときにエラー文を出す。
 if ! [ "$n" -ge  "1" -a "$m" -ge  "1" ] 2>/dev/null ;
 then echo "ERROR: PLZ INPUT NATURAL NUMBER"
  exit 1
